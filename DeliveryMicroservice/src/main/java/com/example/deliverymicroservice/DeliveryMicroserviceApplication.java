8057d4e09bf95adbe59dcf281a5a9f02aa236a41
zickrain <lastpeace14@gmail.com>
2021-10-26 17:35:33 +0600

    Adding Hystrix

--- DeliveryMicroservice/src/main/java/com/example/deliverymicroservice/DeliveryMicroserviceApplication.java
+++ DeliveryMicroservice/src/main/java/com/example/deliverymicroservice/DeliveryMicroserviceApplication.java
@@ -2,10 +2,14 @@ package com.example.deliverymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@EnableCircuitBreaker
+//@EnableHystrix
 public class DeliveryMicroserviceApplication {
 
     public static void main(String[] args) {
--- DeliveryMicroservice/src/main/java/com/example/deliverymicroservice/service/DeliveryService.java
+++ DeliveryMicroservice/src/main/java/com/example/deliverymicroservice/service/DeliveryService.java
@@ -3,8 +3,12 @@ package com.example.deliverymicroservice.service;
 import com.example.deliverymicroservice.model.dto.DeliveryOrderRequestDto;
 import com.example.deliverymicroservice.model.entity.DeliveryOrder;
 import com.example.deliverymicroservice.repository.DeliveryRepository;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +17,8 @@ import java.time.LocalDateTime;
 public class DeliveryService {
 
     private final DeliveryRepository deliveryRepository;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     public DeliveryOrder getDeliveryOrderByOrderId(Long orderId) {
         return deliveryRepository.getDeliveryOrderByOrderId(orderId);
@@ -25,5 +31,24 @@ public class DeliveryService {
         deliveryOrder.setTrackingNumber(dto.getTrackingNumber());
         deliveryOrder.setCreatedAt(LocalDateTime.now());
         deliveryRepository.save(deliveryOrder);
+    }
+
+//    @HystrixCommand(
+//            fallbackMethod = "getUserByIdFallback",
+//            threadPoolKey = "getUserById",
+//            threadPoolProperties = {
+//                    @HystrixProperty(name="coreSize", value="100"),
+//                    @HystrixProperty(name="maxQueueSize", value="50"),
+//            })
+//    public DeliveryOrder getDeliveryOrder(Long id) {
+//        return restTemplate.getForObject("http://delivery-service/delivery/get-by-order" + id, DeliveryOrder.class);
+//    }
+
+    public DeliveryOrder getDeliveryOrderFallback(Long id) {
+        DeliveryOrder deliveryOrder = new DeliveryOrder();
+        deliveryOrder.setAddress("Address is not available: Service Unavailable");
+        deliveryOrder.setOrderId(0L);
+        deliveryOrder.setTrackingNumber("Tracking number is not available: Service Unavailable");
+        return deliveryOrder;
     }
 }
