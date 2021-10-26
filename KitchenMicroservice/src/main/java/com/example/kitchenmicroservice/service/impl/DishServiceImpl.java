ef7c6013533351882d75d52683ca118d88e7c830
timisnotcalm <bzik.zik@gmail.com>
2021-10-26 17:51:04 +0600

    Hystrix commit

--- KitchenMicroservice/src/main/java/com/example/kitchenmicroservice/service/impl/DishServiceImpl.java
+++ KitchenMicroservice/src/main/java/com/example/kitchenmicroservice/service/impl/DishServiceImpl.java
@@ -3,8 +3,11 @@ package com.example.kitchenmicroservice.service.impl;
 import com.example.kitchenmicroservice.model.Dish;
 import com.example.kitchenmicroservice.repository.DishRepository;
 import com.example.kitchenmicroservice.service.DishService;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class DishServiceImpl implements DishService {
+
+//    @Autowired
+//    private RestTemplate restTemplate;
     private final DishRepository dishRepository;
 
     @Override
@@ -22,6 +28,15 @@ public class DishServiceImpl implements DishService {
     @Override
     public Double getPrice(Long dishId) {
         return getDish(dishId).getPrice();
+    }
+
+//    @HystrixCommand(fallbackMethod = "getPriceByDishIdFallback")
+//    public Double getPriceByDishId(Long id) {
+//        return restTemplate.getForObject("http://kitchen-service/dishes/get-by-id/" + id, Double.class);
+//    }
+
+    public Double getPriceByDishIdFallback(Long id) {
+        return 0.0;
     }
 
     @Override
