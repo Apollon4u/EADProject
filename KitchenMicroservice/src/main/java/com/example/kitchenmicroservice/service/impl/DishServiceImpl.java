f5f79388f8b51d8c56eb1a1cae5d7fe1360cd79e
timisnotcalm <bzik.zik@gmail.com>
2021-11-19 23:49:25 +0600

    Security in microservice data transfer

--- KitchenMicroservice/src/main/java/com/example/kitchenmicroservice/service/impl/DishServiceImpl.java
+++ KitchenMicroservice/src/main/java/com/example/kitchenmicroservice/service/impl/DishServiceImpl.java
@@ -5,7 +5,11 @@ import com.example.kitchenmicroservice.repository.DishRepository;
 import com.example.kitchenmicroservice.service.DishService;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,20 +39,20 @@ public class DishServiceImpl implements DishService {
 //        return restTemplate.getForObject("http://kitchen-service/dishes/get-by-id/" + id, Double.class);
 //    }
 
-    @HystrixCommand(
-            fallbackMethod = "getDishByIdFallback",
-            threadPoolKey = "getDishById",
-            threadPoolProperties = {
-                    @HystrixProperty(name="coreSize", value="100"),
-                    @HystrixProperty(name="maxQueueSize", value="50"),
-            },
-            commandKey = "getOrderById",
-            commandProperties = {
-                    @HystrixProperty(name = "requestVolumeThreshold", value = "10"),
-                    @HystrixProperty(name = "errorThresholdPercentage", value = "50"),
-                    @HystrixProperty(name = "sleepWindowInMilliseconds", value = "3000")
-            }
-    )
+//    @HystrixCommand(
+//            fallbackMethod = "getDishByIdFallback",
+//            threadPoolKey = "getDishById",
+//            threadPoolProperties = {
+//                    @HystrixProperty(name="coreSize", value="100"),
+//                    @HystrixProperty(name="maxQueueSize", value="50"),
+//            },
+//            commandKey = "getOrderById",
+//            commandProperties = {
+//                    @HystrixProperty(name = "requestVolumeThreshold", value = "10"),
+//                    @HystrixProperty(name = "errorThresholdPercentage", value = "50"),
+//                    @HystrixProperty(name = "sleepWindowInMilliseconds", value = "3000")
+//            }
+//    )
     public Dish getDishById(Long id) {
         return restTemplate.getForObject("http://kitchen-service/dishes/get-by-id" + id, Dish.class);
     }
@@ -67,6 +71,17 @@ public class DishServiceImpl implements DishService {
     @Override
     public void createDish(Dish dish) {
         dishRepository.save(dish);
+    }
+
+    @Override
+    public String getProductName(Long id) {
+        String apiCredentials = "rest-client:p@ssword";
+        String base64Credentials = new String(Base64.encodeBase64(apiCredentials.getBytes()));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Credentials);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        return restTemplate.exchange("http://product-service/product/get-product-name/" + id,
+                HttpMethod.GET, entity, String.class).getBody();
     }
 
     @Override
